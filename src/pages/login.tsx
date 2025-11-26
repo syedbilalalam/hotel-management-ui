3b2620da0d23255c8ac7c387b40c2b53b3a3c529
Syed Bilal Alam <syedbilalalam2006@gmail.com>
2025-11-27 02:21:05 +0500

    Feat: Face id support added

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,122 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from 'react';
+import CenteredBody from '@src/pages/components/centered_body';
+import { SecBtn } from '@src/pages/components/form_btns';
 import "@src/assets/styles/login.css"; // import CSS file
 
+type BoolSetter = (val: boolean) => void;
+
 interface LoginPageProps {
-    setLoginState: (val: boolean) => void;
+    setLoginState: BoolSetter;
     userEmail: string;
     userPassword: string;
 }
 
+interface LoginProps extends LoginPageProps {
+    setFaceIdPage: BoolSetter;
+}
 
-export default function LoginPage({ setLoginState, userEmail, userPassword }: LoginPageProps) {
+interface FaceIdProps {
+    setFaceIdPage: BoolSetter;
+}
+
+function FaceId(props: FaceIdProps) {
+    const [scanState, setScanState] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(false);
+    const [cameraState, setCameraState] = useState(true);
+    const vidElem = useRef<HTMLVideoElement>(null);
+    const stream = useRef<MediaStream>(null);
+    const scanInterval = useRef<number>(null);
+    const scanAnimationElem = useRef<HTMLDivElement>(null);
+
+    const startCamera = async () => {
+        try {
+            stream.current = await navigator.mediaDevices.getUserMedia({
+                video: { facingMode: 'user' },  // or { facingMode: "user" } for front camera (mobile)
+                audio: false
+            });
+
+            vidElem.current!.srcObject = stream.current; // Set camera stream
+            setScanState(true);
+        } catch {
+            alert('Camera not found!');
+            props.setFaceIdPage(false);
+        }
+    }
+
+    useEffect(() => {
+        startCamera();
+    }, []);
+
+    useEffect(() => {
+        if (cameraState) return;
+
+        if (stream.current)
+            stream.current.getTracks().forEach(track => track.stop());
+
+        // Switching back to login page
+        setScanState(false);
+        props.setFaceIdPage(false);
+    }, [cameraState]);
+
+    useEffect(() => {
+        console.log(scanState);
+        if (scanState && !errorMsg) {
+            let count = 0;
+
+            scanInterval.current = setInterval(() => {
+                if (count % 2 === 0)
+                    scanAnimationElem.current!.classList.add('focus');
+                else
+                    scanAnimationElem.current!.classList.remove('focus');
+                count++;
+            }, 500);
+        }
+        else if (scanInterval.current) {
+            clearInterval(scanInterval.current);
+            scanInterval.current = null;
+        }
+
+        if (!scanState || errorMsg) return;
+
+        setErrorMsg(false);
+
+        setTimeout(() => {
+            setErrorMsg(true);
+        }, 5000);
+
+    }, [scanState, errorMsg]);
+
+    return (
+        <div className={'faceIdComp'}>
+            <CenteredBody maxWidth={1000}>
+                <div className={'title'}>
+                    <span>FACE ID</span>
+                    <img src={'/icons/svg/crop_free.svg'} alt={'Scan'} />
+                </div>
+                <div className={'camContainer'}>
+                    <div className={'camBorder'} ref={scanAnimationElem}>
+                        <video id={'cam'} ref={vidElem} autoPlay={true} playsInline={true}></video>
+                    </div>
+                </div>
+                {errorMsg ? (
+                    <div className={'errorContainer'}>
+                        <div className={'error'}>
+                            <img src={'/icons/svg/filter_center_focus.svg'} alt={'icon'} />
+                            <span>Face didn't match, Try again with credentials</span>
+                        </div>
+                        <div className={'backLogin'}>
+                            <SecBtn onClick={() => {
+                                setCameraState(false);
+                            }}>Login via Credentials</SecBtn>
+                        </div>
+                    </div>
+                ) : (<></>)}
+            </CenteredBody>
+        </div>
+    )
+}
+
+function Login(props: LoginProps) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -19,8 +127,8 @@ export default function LoginPage({ setLoginState, userEmail, userPassword }: Lo
                 console.log("Email:", email);
                 console.log("Password:", password);
 
-                if (email === userEmail && password === userPassword) {
-                    setLoginState(true);
+                if (email === props.userEmail && password === props.userPassword) {
+                    props.setLoginState(true);
                 }
                 else {
                     alert('Wrong information entered');
@@ -48,14 +156,39 @@ export default function LoginPage({ setLoginState, userEmail, userPassword }: Lo
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
-                        placeholder="••••••••"
+                        placeholder="*********"
                     />
                 </div>
 
                 <button type="submit" className="login-button">
                     Login
                 </button>
+
+                <button
+                    type="button" className="login-button faceid"
+                    onClick={() => {
+                        props.setFaceIdPage(true);
+                    }}
+                >
+                    Continue with Face Id
+                </button>
             </form>
         </div>
+    )
+}
+
+
+export default function LoginPage(props: LoginPageProps) {
+
+    const [faceIdPage, setFaceIdPage] = useState(false);
+
+    return (
+        <>
+            {faceIdPage ? (
+                <FaceId {...{ setFaceIdPage }} />
+            ) : (
+                <Login {...{ ...props, setFaceIdPage }} />
+            )}
+        </>
     );
 }
